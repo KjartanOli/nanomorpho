143d164df09e87782e4ebba98b31eb9baf3e007d
Kjartan Oli Agustsson <koa20@hi.is>
2024-02-05 15:26:53 +0000

    Add tests
    * test.s: New file.

---
+++ test.s
@@ -0,0 +1,23 @@
+f(_a, b){
+	var a;
+	return a;
+}
+
+g(x) {
+	return x;
+}
+
+h(x,y) {
+	return x + y;
+}
+
+a() {
+var x;
+if ((x)) { y; }
+elsif (a + b) { x = y; }
+else { z; };
+}
+
+a(g) {
+	while () {}
+}
