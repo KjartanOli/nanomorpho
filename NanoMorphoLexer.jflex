5510fba94a12bd537daedf46c2c57b13039431b0
Kjartan Oli Agustsson <koa20@hi.is>
2024-02-16 13:12:56 +0000

    Start on parser

--- NanoMorphoLexer.jflex
+++ NanoMorphoLexer.jflex
@@ -2,103 +2,172 @@
 
 %public
 %class NanoMorphoLexer
-%implements NanoMorpho.Lexer
 %unicode
 %line
 %column
 %byaccj
 
 %{
+// We will have one and only one lexer
+private static NanoMorphoLexer lexer;
 
-	private static NanoMorphoLexer lexer;
-	String lexeme1, lexeme2;
-	private static int token1, token2;
-	private int line1, colum1, line2, colum2;
+// We will maintain a data invariant
+// that we have information about the
+// next token and the following token.
+// After initialization and before and
+// after each public operation we have:
+//  * token1 is the next unprocessed token
+//    and lexeme1, line1, and column1 are
+//    corresponding to that token.
+//  * token2 is the following unprocessed token
+//    and lexeme2, line2, and column2 are
+//    corresponding to that token.
+// This data invariant is established in the
+// static initialization method startLexer
+// and is maintained each time we use the
+// lexer operations to advance in the input.
+// A special case is at end of file, in which
+// case token2, lexeme2, line2, and column2
+// are irrelevant and should be ignored.
+// Each lexer action, i.e. a call to lexer.yylex()
+// returns the appropriate token and also updates
+// the variable lexeme2 to contain the underlying
+// lexeme.
+private static int token1, token2;
+private static String lexeme1, lexeme2;
+private static int line1, column1, line2, column2;
 
-	public static void startLexer( String filename ) throws Exception
-	{
-    	   startLexer(new FileReader(filename));
-	}
+public static void startLexer( String filename ) throws Exception
+{
+    startLexer(new FileReader(filename));
+}
 
-	public static void startLexer( Reader r ) throws Exception
-	{
-	    lexer = new NanoMorphoLexer(r);
-	    token1 = lexer.yylex(); // changes lexeme2
-	    lexeme1 = lexeme2;
-	    line1 = lexer.yyline;
-	    column1 = lexer.yycolumn;
-	    if( token1 == NanoMorphoParser.EOF ) return;
-	    token2 = lexer.yylex(); // changes lexeme2
-	    line2 = lexer.yyline;
-	    column2 = lexer.yycolumn;
-	}
+public static void startLexer( Reader r ) throws Exception
+{
+    lexer = new NanoMorphoLexer(r);
+    token1 = lexer.yylex(); // changes lexeme2
+    lexeme1 = lexeme2;
+    line1 = lexer.yyline;
+    column1 = lexer.yycolumn;
+    if( token1 == NanoMorphoParser.EOF ) return;
+    token2 = lexer.yylex(); // changes lexeme2
+    line2 = lexer.yyline;
+    column2 = lexer.yycolumn;
+}
 
+public static String advance() throws Exception
+{
+    String res = lexeme1;
+    token1 = token2;
+    lexeme1 = lexeme2;
+    line1 = line2;
+    column1 = column2;
+    if( token2 == NanoMorphoParser.EOF ) return res;
+    token2 = lexer.yylex();
+    line2 = lexer.yyline;
+    column2 = lexer.yycolumn;
+    return res;
+}
 
-	public static String advance(){
-		String res = lexeme1;
-		token1 = token2;
-		lexem1 = lexeme2
-		if(token1 == EOF) return; // Tíma bundið nafn fyrir end of file
-		token2 =lexer.yylex;
-	}
+public static int getLine()
+{
+    return line1+1;
+}
 
-	public String getLVal()
-	{
-		return yylval;
-	}
+public static int getColumn()
+{
+    return column1+1;
+}
 
-	public void yyerror( String error )
-	{
-		System.err.println("Error:  "+error);
-		System.err.println("Lexeme: "+yylval);
-		System.err.println("Line:   "+(yyline+1));
-		System.err.println("Column: "+(yycolumn+1));
-		System.exit(1);
-	}
+public static int getToken()
+{
+    return token1;
+}
 
-	public void show( String token, String lexeme )
-	{
-		System.out.print("Token: "+token);
-		System.out.print(", Lexeme: "+lexeme);
-		System.out.print(", Line: "+(yyline+1));
-		System.out.println(", Column: "+(yycolumn+1));
-	}
+public static String getTokenName()
+{
+    return tokname(token1);
+}
 
-	public String getTokenName( int token )
-	{
-		switch( token )
-		{
-		case LITERAL:   return "LITERAL";
-		case NAME:      return "NAME";
-		case IF:        return "IF";
-		case ELSEIF:    return "ELSEIF";
-		case ELSE:      return "ELSE";
-		case WHILE:     return "WHILE";
-		case YYERRCODE: return "YYERRCODE";
-		case '(':       return "'('";
-		case ')':       return "')'";
-		case '{':       return "'{'";
-		case '}':       return "'}'";
-		}
-		return "unknown";
-	}
+public static int getToken2()
+{
+    return token2;
+}
 
-	public int getLine() { return yyline+1; }
-	public int getColumn() { return yycolumn+1; }
+public static String getLexeme()
+{
+    return lexeme1;
+}
 
-	public String over(int token){
-	       if(token1!=token) expcted(token);
-	       String res = lexeme1;
-	       advance();
-	       return res;
-	}
+public static void expected( int tok )
+{
+    expected(tokname(tok));
+}
 
-	public String over(char token){
-	       if(token1!=token) expcted(token);
-	       String res = lexeme1;
-	       advance();
-	       return res;
-	}
+public static void expected( char tok )
+{
+    expected("'"+tok+"'");
+}
+
+public static void expected( String tok )
+{
+    throw new Error("Expected "+tok+", found '"+lexeme1+"' near line "+(line1+1)+", column "+(column1+1));
+}
+
+private static String tokname( int tok )
+{
+    if( tok<1000 ) return "'"+(char)tok+"'";
+    switch( tok )
+    {
+    case NanoMorphoParser.IF:
+        return "IF";
+    case NanoMorphoParser.ELSE:
+        return "ELSE";
+    case NanoMorphoParser.ELSIF:
+        return "ELSIF";
+    case NanoMorphoParser.WHILE:
+        return "WHILE";
+    case NanoMorphoParser.VAR:
+        return "VAR";
+    case NanoMorphoParser.RETURN:
+        return "RETURN";
+    case NanoMorphoParser.NAME:
+        return "NAME";
+    case NanoMorphoParser.LITERAL:
+        return "LITERAL";
+    case NanoMorphoParser.OP1:
+        return "OP1";
+    case NanoMorphoParser.OP2:
+        return "OP2";
+    case NanoMorphoParser.OP3:
+        return "OP3";
+    case NanoMorphoParser.OP4:
+        return "OP4";
+    case NanoMorphoParser.OP5:
+        return "OP5";
+    case NanoMorphoParser.OP6:
+        return "OP6";
+    case NanoMorphoParser.OP7:
+        return "OP7";
+    }
+    throw new Error();
+}
+
+public static String over( int tok ) throws Exception
+{
+    if( token1!=tok ) expected(tok);
+    String res = lexeme1;
+    advance();
+    return res;
+}
+
+public static String over( char tok ) throws Exception
+{
+    if( token1!=tok ) expected(tok);
+    String res = lexeme1;
+    advance();
+    return res;
+}
 %}
 
   /* Reglulegar skilgreiningar */
@@ -139,6 +208,11 @@ _OPNAME=[:&|<>=+\-*/%!?~\^]+
 "else" {
 	yylval = yytext();
 	return ELSE;
+}
+
+"fun" {
+	yylval = yytext();
+	return FUN;
 }
 
 "var" {
---
+++ NanoMorphoParser.java
@@ -0,0 +1,339 @@
+import java.util.Vector;
+import java.util.HashMap;
+
+public class NanoMorphoParser
+{
+    final static int YYERRCODE = -1;
+    final static int EOF = 0;
+    final static int IF = 1001;
+    final static int ELSE = 1002;
+    final static int ELSIF = 1003;
+    final static int WHILE = 1004;
+    final static int VAR = 1005;
+    final static int RETURN = 1006;
+    final static int NAME = 1007;
+    final static int LITERAL = 1008;
+    final static int OP1 = 1009;
+    final static int OP2 = 1010;
+    final static int OP3 = 1011;
+    final static int OP4 = 1012;
+    final static int OP5 = 1013;
+    final static int OP6 = 1014;
+    final static int OP7 = 1015;
+	final static int FUN = 1016;
+
+    static String advance() throws Exception
+    {
+			return NanoMorphoLexer.advance();
+		}
+    
+    static String over( int tok ) throws Exception
+    {
+			return NanoMorphoLexer.over(tok);
+		}
+    
+    static String over( char tok ) throws Exception
+    {
+			return NanoMorphoLexer.over(tok);
+		}
+    
+    static int getToken()
+    {
+			return NanoMorphoLexer.getToken();
+		}
+    
+    static String getTokenName()
+    {
+			return NanoMorphoLexer.getTokenName();
+		}
+    
+    static String getLexeme()
+    {
+			return NanoMorphoLexer.getLexeme();
+		}
+    
+    static int getToken2()
+    {
+			return NanoMorphoLexer.getToken2();
+		}
+    
+    static int getLine()
+    {
+			return NanoMorphoLexer.getLine();
+		}
+    
+    static int getColumn()
+    {
+			return NanoMorphoLexer.getColumn();
+		}    
+
+    static void expected( int tok )
+    {
+			NanoMorphoLexer.expected(tok);
+		}
+    
+    static void expected( char tok )
+    {
+			NanoMorphoLexer.expected(tok);
+		}
+
+    static void expected( String str )
+    {
+			NanoMorphoLexer.expected(str);
+		}
+        
+    // You will not need the symbol table until you do the compiler proper.
+    // The symbol table consists of the following two variables.
+    private static int varCount;
+    private static HashMap<String,Integer> varTable;
+
+    // Adds a new variable to the symbol table.
+    // Throws Error if the variable already exists.
+    private static void addVar( String name )
+    {
+			if( varTable.get(name) != null )
+				expected("undeclared variable name");
+			varTable.put(name,varCount++);
+		}
+
+    // Finds the location of an existing variable.
+    // Throws Error if the variable does not exist.
+    private static int findVar( String name )
+    {
+			Integer res = varTable.get(name);
+			if( res == null )
+				expected("declared variable name");
+			return res;
+		}
+    
+    static public void main( String[] args ) throws Exception
+    {
+			Vector<Object[]> code = null;
+			try
+        {
+					NanoMorphoLexer.startLexer(args[0]);
+					code = program();
+					if( getToken() != EOF ) expected("end of file");
+				}
+        catch( Throwable e )
+        {
+					System.out.println(e.getMessage());
+					System.exit(1);
+				}
+			// For the parser you do not need this line:
+			generateProgram(args[0],code);
+		}
+
+    static Vector<Object[]> program() throws Exception {
+			while (token1 != EOF)
+				function();
+			return null;
+	}
+
+	static Object[] function() throws Exception {
+			varCount = 0;
+			varTable = new HashMap<String,Integer>();
+
+			over(FUN);
+			over(NAME);
+			over('(');
+			parameter_list();
+			over(')');
+			function_body();
+
+			// We're done with this symbol table so we'll erase it
+			varTable = null;
+			return null;
+	}
+
+	static Object[] declaration_list() throws Exception {
+		while (token1 == VAR) {
+			decl();
+			over(';');
+		}
+	}
+
+	static Object[] function_body() throws Exception {
+		over('{');
+		declaration_list();
+		expression_list();
+		over('}');
+	}
+
+    // decl = 'var', NAME, { ',' NAME } ;
+    static void decl() throws Exception
+    {
+			over(VAR);
+			addVar(over(NAME));
+			while( getToken() == ',' )
+				{
+					over(',');
+					addVar(over(NAME));
+				}
+		}
+
+    static Object[] expr() throws Exception
+    {
+			if (token1 == RETURN) {
+				over(RETURN);
+				expr();
+			}
+			else if (token1 == NAME && token2 == '=') {
+				over(NAME);
+				over('=');
+				expr();
+			}
+			else {
+				binopexpr();
+			}
+			return null;
+		}
+
+    // For the parser you do not need the pri argument and
+    // in the compiler you only need it if you wish to
+    // distinguish priorities of operators
+    static Object[] binopexpr( int pri ) throws Exception {
+		smallexpr();
+		while (token1 == OPNAME) {
+			over(OPNAME);
+			smallexpr();
+		}
+		return null;
+	}
+    
+    static Object[] smallexpr() throws Exception {
+		switch (token1) {
+			case NAME:
+				over(NAME);
+				if (token1 == '(') {
+					over('(');
+					expression_list();
+					over(')');
+				}
+				break;
+			case OPNAME:
+				over(OPNAME);
+				smallexpr();
+				break;
+			case LITERAL:
+				over(LITERAL);
+				break;
+			case '(':
+				over('(');
+				expr();
+				over(')');
+				break;
+			case IFEXPR:
+				ifexpr();
+				break;
+			case WHILE:
+				over(WHILE);
+				expr();
+				body();
+				break;
+		}
+		//...
+		return null;
+	}
+
+    // if-expression syntax without 'elsif'
+    //
+    // ifexpr = 'if', expr, body, [ 'else', ( ifexpr | body ) ] ;
+    static Object[] ifexpr() throws Exception
+    {
+			over(IF);
+			Object[] cond = expr();
+			Object[] thenpart = body();
+			if( getToken() != ELSE )
+				return new Object[]{"IF1",cond,thenpart};
+			over(ELSE);
+			if( getToken() == IF )
+				return new Object[]{"IF2",cond,thenpart,ifexpr()};
+			else if( getToken() == '{' )
+				return new Object[]{"IF2",cond,thenpart,body()};
+			else
+				expected("'if' or '{' following 'else'");
+			throw new Error("This can't happen");
+		}
+    
+    /*
+    // Alternative syntax with 'elsif'.
+       // Slightly more complicated and requires that
+       // the caller verifies beforehand that 'if'
+       // and 'elsif' are used in their proper places.
+
+    // ifexpr = 'if', expr, body, [ ifrest ] ;
+       // ifrest = 'else', body | 'elsif', expr, body, [ ifrest ] ;
+       static Object[] ifexpr() throws Exception
+       {
+       if( getToken() == ELSIF ) over(ELSIF) else over(IF);
+       Object[] cond = expr();
+       Object[] thenpart = body();
+       if( getToken() != ELSE && getToken() != ELSIF )
+       return new Object[]{"IF1",cond,thenpart};
+       if( getToken() == ELSIF )
+       return new Object[]{"IF2",cond,thenpart,ifexpr()};
+       over(ELSE);
+       return new Object[]{"IF2",cond,thenpart,body()};
+       }
+     */
+    
+    static Object[] body() throws Exception
+    {
+			//...
+			return null;
+		}
+    
+    // None of the following is needed in the parser
+    static void generateProgram( String filename, Vector<Object[]> funs )
+    {
+			String programname = filename.substring(0,filename.lastIndexOf('.'));
+			emit("\"%s.mexe\" = main in",programname);
+			emit("!");
+			emit("{{");
+			for( Object[] f: funs ) generateFunction(f);
+			emit("}}");
+			emit("*");
+			emit("BASIS;");
+		}
+    
+    static void emit( String fmt, Object... args )
+    {
+			System.out.format(fmt,args);
+			System.out.println();
+		}
+    
+    static void generateFunction( Object[] fun )
+    {
+			//...
+		}
+    
+    // All existing labels, i.e. labels the generated
+    // code that we have already produced, should be
+    // of form
+    //    _xxxx
+    // where xxxx corresponds to an integer n
+    // such that 0 <= n < nextLab.
+    // So we should update nextLab as we generate
+    // new labels.
+    // The first generated label would be _0, the
+    // next would be _1, and so on.
+    private static int nextLab = 0;
+    
+    // Returns a new, previously unused, label.
+    // Useful for control-flow expressions.
+    static String newLabel()
+    {
+			return "_"+(nextLab++);
+		}
+    
+    static void generateExpr( Object[] e )
+    {
+			//...
+		}
+    
+    static void generateBody( Object[] bod )
+    {
+			//...
+		}
+}
